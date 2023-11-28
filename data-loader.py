ed42ed9657ad45e7575cddbfc482eff71cc8d56b
Christopher Brown <cjb4@williams.edu>
2023-11-28 08:49:29 -0500

    commit

--- data-loader.py
+++ data-loader.py
@@ -1,2 +1,27 @@
 import pandas as pd
 import numpy as np
+import os
+
+def load_data():
+    paths_to_files = []
+    directory = "data"
+
+    for name in os.scandir(directory):
+        if name.is_dir():
+            for obj in os.scandir(name):
+                if obj.is_file():
+                    paths_to_files.append(obj.path)
+    
+    file_objects = []
+
+
+    data = pd.read_csv(paths_to_files[0])
+    
+    print("test1")
+    data = pd.get_dummies(data)
+    print("test2")
+    
+    for col in data.columns:
+        print(col)
+
+load_data()
